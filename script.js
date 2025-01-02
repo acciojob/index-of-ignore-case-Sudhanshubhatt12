904cfbfe1d19f64c4e729d6ed653c36ae7b8afda
Sudhanshu Bhatt <101445771+Sudhanshubhatt12@users.noreply.github.com>
2025-01-02 17:53:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+  // If the substring is empty, return 0 as the first occurrence is at index 0
+  if (s2 === '') return 0;
+
+  // Convert both strings to lowercase for case-insensitive comparison
+  const lowerS1 = s1.toLowerCase();
+  const lowerS2 = s2.toLowerCase();
+
+  // Find the index of the first occurrence of the substring in the main string
+  return lowerS1.indexOf(lowerS2);
 }
 
-// Please do not change the code below
+// Do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
+
